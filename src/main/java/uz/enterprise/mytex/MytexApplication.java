294d5c0a275fde57fae2a79210e5b923a9d67fba
Shohjahon Rahmataliyev <r.shohjahon@gmail.com>
2022-10-22 14:28:55 +0500

    initial commit

---
+++ src/main/java/uz/enterprise/mytex/MytexApplication.java
@@ -0,0 +1,13 @@
+package uz.enterprise.mytex;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MytexApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MytexApplication.class, args);
+    }
+
+}
---
+++ src/main/java/uz/enterprise/mytex/common/Generated.java
@@ -0,0 +1,19 @@
+package uz.enterprise.mytex.common;
+
+import java.lang.annotation.Documented;
+import java.lang.annotation.Retention;
+import java.lang.annotation.Target;
+
+import static java.lang.annotation.ElementType.METHOD;
+import static java.lang.annotation.ElementType.TYPE;
+import static java.lang.annotation.RetentionPolicy.RUNTIME;
+
+/**
+ *  Methods or classes with @Generated annotation are not
+ *  included in jacoco code coverage report
+ */
+@Documented
+@Retention(RUNTIME)
+@Target({TYPE, METHOD})
+public @interface Generated {
+}
